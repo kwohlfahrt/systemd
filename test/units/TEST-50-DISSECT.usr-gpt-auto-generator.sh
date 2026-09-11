6520b1f955190a0ba3799bc35b1b62538e5e1098
Kai Wohlfahrt <kai@proximabio.com>
2026-09-11 10:29:41 +0200

    fixup! Add test for gpt-auto-generator

--- test/units/TEST-50-DISSECT.usr-gpt-auto-generator.sh
+++ test/units/TEST-50-DISSECT.usr-gpt-auto-generator.sh
@@ -53,39 +53,40 @@ udevadm wait --timeout=60 --settle --initialized=no "$LOOP"p1 "$LOOP"p2
 
 # Recreate a root=tmpfs system whose /usr/ is backed by a GPT partition and overlaid by systemd-sysext.
 # Run the generator as PID 1 because generators deliberately do nothing in containers.
-unshare --mount --pid --fork --mount-proc bash -euxo pipefail -c '
-    mount --make-rprivate /
+unshare --mount --pid --fork --mount-proc \
+    bash -euxo pipefail -s -- "$ROOT" "$OUTPUT" "$WORK_DIR" "$LOOP" "$GENERATOR_BIN" <<'EOF'
+mount --make-rprivate /
 
-    mount --types tmpfs tmpfs "$1"
-    mkdir -p "$1"/{dev,proc,run/generator-output,sys,usr}
-    mount --bind /dev "$1/dev"
-    mount --bind /proc "$1/proc"
-    mount --bind /sys "$1/sys"
-    mount --bind "$2" "$1/run/generator-output"
+mount --types tmpfs tmpfs "$1"
+mkdir -p "$1"/{dev,proc,run/generator-output,sys,usr}
+mount --bind /dev "$1/dev"
+mount --bind /proc "$1/proc"
+mount --bind /sys "$1/sys"
+mount --bind "$2" "$1/run/generator-output"
 
-    mkdir -p "$3/usr-upper" "$3/usr-work"
-    mount --types overlay overlay \
-        --options lowerdir=/usr,upperdir="$3/usr-upper",workdir="$3/usr-work" \
-        "$1/usr"
+mkdir -p "$3/usr-upper" "$3/usr-work"
+mount --types overlay overlay \
+    --options lowerdir=/usr,upperdir="$3/usr-upper",workdir="$3/usr-work" \
+    "$1/usr"
 
-    for directory in bin lib lib64 sbin; do
-        if [[ -e "/$directory" ]]; then
-            mkdir -p "$1/$directory"
-            mount --bind "/$directory" "$1/$directory"
-        fi
-    done
+for directory in bin lib lib64 sbin; do
+    if [[ -e "/$directory" ]]; then
+        mkdir -p "$1/$directory"
+        mount --bind "/$directory" "$1/$directory"
+    fi
+done
 
-    mkdir -p "$1/usr/.systemd-sysext"
-    cat "/sys/class/block/${4##*/}p1/dev" >"$1/usr/.systemd-sysext/backing"
+mkdir -p "$1/usr/.systemd-sysext"
+cat "/sys/class/block/${4##*/}p1/dev" >"$1/usr/.systemd-sysext/backing"
 
-    export container=
-    export SYSTEMD_IN_INITRD=0
-    export SYSTEMD_PROC_CMDLINE="root=tmpfs systemd.gpt_auto=yes"
-    exec chroot "$1" "$5" \
-        /run/generator-output/normal \
-        /run/generator-output/early \
-        /run/generator-output/late
-' bash "$ROOT" "$OUTPUT" "$WORK_DIR" "$LOOP" "$GENERATOR_BIN"
+export container=
+export SYSTEMD_IN_INITRD=0
+export SYSTEMD_PROC_CMDLINE="root=tmpfs systemd.gpt_auto=yes"
+exec chroot "$1" "$5" \
+    /run/generator-output/normal \
+    /run/generator-output/early \
+    /run/generator-output/late
+EOF
 
 test -f "$OUTPUT/late/home.mount"
 WHAT="$(sed -n 's/^What=//p' "$OUTPUT/late/home.mount")"
